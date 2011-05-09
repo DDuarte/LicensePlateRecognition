6a79b72325c1c6b7fa08f5e793a9d1d7da8c2a30
Zephiro309 <zephiro309@gmail.com>
2011-05-09 14:20:06 +0100

    Video mode update, missing libraries and headers :/

---
+++ src/video_test.cpp
@@ -0,0 +1,18 @@
+#include "highgui.h"
+
+int main(int argc, char** argv)
+{
+	cvNamedWindow("Camara", CV_WINDOW_AUTOSIZE);
+	CvCapture* capture = cvCreateCameraCapture(-1);
+	IplImage* frame;
+	while(1)
+	{
+		frame = cvQueryFrame(capture);
+		if(!frame) break;
+		cvShowImage("Camara", frame);
+		char c = cvWaitKey(66);
+		if(c == 27) break;
+	}
+	cvReleaseCapture(&capture);
+	cvDestroyWindow("Camara");
+}
